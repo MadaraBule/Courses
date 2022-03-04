42f489be4fb75013f8f3f797154a43320a5bd266
Madara  Bule <madara8a@gmail.com>
2022-03-04 16:02:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ ASSIGNMENT RULES
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let = l1;
+let l2;
 
+const calculateArea = function (a, b) {
+  return a * b;
+};
+console.log(calculateArea(l1, l2));
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
