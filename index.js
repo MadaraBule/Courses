7f03755f720336ac841d094dad7ec58e1c4ebf25
Madara  Bule <madara8a@gmail.com>
2022-03-04 16:43:52 +0200

    Update index.js
    forgot to check console

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const boundary = function (n) {
     return false;
   }
 };
-
+console.log(boundary(44));
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
